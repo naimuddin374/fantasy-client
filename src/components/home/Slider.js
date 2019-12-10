ed6559739e8ef503cab522e9c694a4053eeea60b
Naim uddin <naimuddin374@gmail.com>
2019-12-10 19:17:26 +0600

    Add loading

--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom'
 import Axios from 'axios'
 import { textLimit } from '../../util/helper'
 import { API_URL } from '../../store/actions/types'
+import Loading from '../layout/Loading'
 
 
 class HomeSlider extends React.Component {
     state = {
-        sliders: {}
+        sliders: {},
+        loading: true
     }
     componentDidMount() {
         Axios.get(`${API_URL}api/home-slider`)
             .then(res => {
                 this.setState({
-                    sliders: res.data
+                    sliders: res.data,
+                    // loading: false
                 })
             })
     }
@@ -26,29 +29,31 @@ class HomeSlider extends React.Component {
             arrows: false,
             autoplay: true,
         }
-        let { sliders } = this.state
+        let { sliders, loading } = this.state
         return (
             <section className="slider-area">
-                <Slider {...settings}>
-                    {Object.keys(sliders).length !== 0 &&
-                        sliders.map(item => (
-                            <div className="hero-single-slide hero-overlay" key={item.id}>
-                                <div className="hero-slider-item bg-img home-slider" style={{ background: `url(${API_URL+item.image})` }}>
-                                    <div className="container">
-                                        <div className="row">
-                                            <div className="col-md-12">
-                                                <div className="hero-slider-content slide-1 text-center">
-                                                    <h1 className="slide-title">{item.title}</h1>
-                                                    <p className="slide-desc text-white">{textLimit(item.description, 150)}</p>	<Link to={`${process.env.PUBLIC_URL}/${item.link}`} className="theme-btn">Get Details</Link>
+                {loading ? <Loading /> :
+                    <Slider {...settings}>
+                        {Object.keys(sliders).length !== 0 &&
+                            sliders.map(item => (
+                                <div className="hero-single-slide hero-overlay" key={item.id}>
+                                    <div className="hero-slider-item bg-img home-slider" style={{ background: `url(${API_URL + item.image})` }}>
+                                        <div className="container">
+                                            <div className="row">
+                                                <div className="col-md-12">
+                                                    <div className="hero-slider-content slide-1 text-center">
+                                                        <h1 className="slide-title">{item.title}</h1>
+                                                        <p className="slide-desc text-white">{textLimit(item.description, 150)}</p>	<Link to={`${process.env.PUBLIC_URL}/${item.link}`} className="theme-btn">Get Details</Link>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
-                    }
-                </Slider>
+                            ))
+                        }
+                    </Slider>
+                }
             </section>
         )
     }
