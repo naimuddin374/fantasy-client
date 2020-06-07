c45f58602d52013d043594c1b3ff761209787491
Naim Uddin <naim.uddin@lubyc.com>
2020-06-07 15:15:11 +0600

    Deal and benefits

--- src/components/home/Benefits.js
+++ src/components/home/Benefits.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { DISCOUNT_TAG } from '../../store/actions/types'
+import { DISCOUNT_TAG, API_URL } from '../../store/actions/types'
+import Loading from '../layout/Loading'
+import Axios from 'axios';
+
 
 class Benefits extends React.Component {
+    state = {
+        data: {},
+        loading: true
+    }
+    componentDidMount() {
+        this.setState({
+            loading: true
+        })
+        Axios.get(`${API_URL}api/deal-benefits`)
+            .then(res => {
+                this.setState({
+                    data: res.data,
+                    loading: false
+                })
+            })
+    }
     render() {
+        let { data, loading } = this.state
         return (
             <section className="deals-benefits-area section-padding-top full-bg">
                 <div className="container">
@@ -14,36 +34,41 @@ class Benefits extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6">
-                            <div className="deails-img relative free-ticke-img deals-benefits-style free-offer">
-                                <Link to={`${process.env.PUBLIC_URL}/ticket`} className="freeticketbg">
-                                    <img src={`${process.env.PUBLIC_URL}/assets/images/deals/free-ticket-img.png`} alt="deals img" />
-                                </Link>
-                                <div className="deails-details-btn  free-ticket">
-                                    <Link to={`${process.env.PUBLIC_URL}/ticket`}>Tickets</Link>
+                    {loading ? <Loading /> : data.length > 0 && data.map(item =>
+                        <div className="row" key={item.id}>
+                            <div className="col-lg-6 col-md-6">
+                                <div className="deails-img relative free-ticke-img deals-benefits-style free-offer">
+                                    <Link to={`${process.env.PUBLIC_URL}/ticket`} className="freeticketbg">
+                                        {/* <img src={`${process.env.PUBLIC_URL}/assets/images/deals/free-ticket-img.png`} alt="deals img" /> */}
+                                        <img src={API_URL + item.image1} alt="deals img" />
+                                    </Link>
+                                    <div className="deails-details-btn  free-ticket">
+                                        <Link to={`${process.env.PUBLIC_URL}/ticket`}>Tickets</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-6">
+                                <div className="deails-img relative deals-benefits-style discount-offer">
+                                    <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`} className="discountbg">
+                                        <img src={API_URL + item.image2} alt="deals img" />
+                                        {/* <img src={`${process.env.PUBLIC_URL}/assets/images/deals/discountdealsimg.png`} alt="deals img" /> */}
+                                    </Link>
+                                    <div className="deails-details-btn">
+                                        <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`}>Discounts Deals</Link>
+                                    </div>
+                                </div>
+                                <div className="deails-img relative deals-benefits-style package-offer">
+                                    <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`} className="dealspackagebg">
+                                        <img src={API_URL + item.image3} alt="deals img" />
+                                        {/* <img src={`${process.env.PUBLIC_URL}/assets/images/deals/dealspackageimg.png`} alt="deals img" /> */}
+                                    </Link>
+                                    <div className="deails-details-btn">
+                                        <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`} className="deals-shape-btn">Package Offers</Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-6">
-                            <div className="deails-img relative deals-benefits-style discount-offer">
-                                <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`} className="discountbg">
-                                    <img src={`${process.env.PUBLIC_URL}/assets/images/deals/discountdealsimg.png`} alt="deals img" />
-                                </Link>
-                                <div className="deails-details-btn">
-                                    <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`}>Discounts Deals</Link>
-                                </div>
-                            </div>
-                            <div className="deails-img relative deals-benefits-style package-offer">
-                                <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`} className="dealspackagebg">
-                                    <img src={`${process.env.PUBLIC_URL}/assets/images/deals/dealspackageimg.png`} alt="deals img" />
-                                </Link>
-                                <div className="deails-details-btn">
-                                    <Link to={`${process.env.PUBLIC_URL}/ticket/${DISCOUNT_TAG}`} className="deals-shape-btn">Package Offers</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    )}
                 </div>
             </section>
         )
